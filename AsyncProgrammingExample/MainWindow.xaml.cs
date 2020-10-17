6a930c8e3ff653bfa8a7c6a230e0f1bdf45657c6
Samiran Kundu <samiran.kundu@nagarro.com>
2020-10-17 23:21:44 +0530

    first commit

---
+++ AsyncProgrammingExample/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AsyncProgrammingExample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+     
+
+        private void Button_ClickLong(object sender, RoutedEventArgs e)
+        {
+            LongTaskButton.IsEnabled = false;
+            Thread.Sleep(2000);
+            LongTaskTextBlock.Text = "Completed long task";
+            LongTaskButton.IsEnabled = true;
+        }
+
+        int count;
+        private void ShortTaskButton_Click(object sender, RoutedEventArgs e)
+        {
+            ShortTaskTextBlock.Text = $"{++count}";
+        }
+
+        private async void LongTaskButton_Click_Async(object sender, RoutedEventArgs e)
+        {
+            LongTaskButton.IsEnabled = false;
+            LongTaskTextBlock.Text = "Starting long tasks";
+
+            var firstLongTask = Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                return "first";
+            });
+
+            var secondLongTask = Task.Run(() =>
+            {
+                Thread.Sleep(2000);
+                return "second";
+            });
+
+            var thirdLongTask = Task.Run(() =>
+            {
+                Thread.Sleep(500);
+                return "third";
+            });
+
+            var results = await Task.WhenAll(firstLongTask, secondLongTask, thirdLongTask);
+
+            LongTaskButton.IsEnabled = true;
+            LongTaskTextBlock.Text = "Completed " + string.Join(", ", results) + " tasks";
+        }
+    }
+}
